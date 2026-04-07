c1a923b48a8c949476e64905b7470ac17b340fb5
yeicor <yeicor@github.com>
2026-04-07 18:43:55 +0200

    Update wasm32-emscripten.cmake

--- vcpkg_triplets/wasm32-emscripten.cmake
+++ vcpkg_triplets/wasm32-emscripten.cmake
@@ -1,8 +1,13 @@
 include("triplets/community/wasm32-emscripten.cmake")
 
 # CUSTOM:
-if(PORT STREQUAL "opencascade")
+set(VCPKG_CMAKE_CONFIGURE_OPTIONS ${VCPKG_CMAKE_CONFIGURE_OPTIONS}
+  -DCMAKE_POSITION_INDEPENDENT_CODE=ON  # -fPIC required for wasm32 module
+)
+
+if(GODOTCPP_THREADS)
   set(VCPKG_CMAKE_CONFIGURE_OPTIONS ${VCPKG_CMAKE_CONFIGURE_OPTIONS}
-    -DCMAKE_POSITION_INDEPENDENT_CODE=ON  # -fPIC required for wasm32 module
+    -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -matomics -mbulk-memory"  # Required for threads support in wasm32
+    -DCMAKE_C_FLAGS="${CMAKE_C_FLAGS} -matomics -mbulk-memory"  # Required for threads support in wasm32
   )
 endif()
